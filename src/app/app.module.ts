26de9fdd14191155fb43bf405598a8fdc386a274
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2021-08-31 14:37:08 -0300

    refactor: add orm config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmConfig } from '../ormconfig';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
   controllers: [AppController],
   providers: [AppService],
+  exports: [TypeOrmModule],
 })
 export class AppModule {}
