1b2c3a7a2d1ad552d8466fb5a30e172a0e585c7f
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2021-09-02 18:22:31 -0300

    refactor: add ssl config

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,7 +1,18 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as ENV from './constants';
 
+let options: TypeOrmModuleOptions = {};
+
+if (ENV.ADD_REJECT_UNAUTHORIZED === 'true') {
+  options = {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  };
+}
+
 export const typeOrmConfig: TypeOrmModuleOptions = {
+  ...options,
   type: 'postgres',
   host: ENV.POSTGRES_HOST,
   username: ENV.POSTGRES_USER,
@@ -9,8 +20,5 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   database: ENV.POSTGRES_DB,
   entities: [`${__dirname}/**/*.entity.{js,ts}`],
   synchronize: ENV.IS_DEV,
-  logging: ENV.IS_DEV,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  logging: ENV.IS_DEV
 };
