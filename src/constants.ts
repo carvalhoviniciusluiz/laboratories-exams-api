650360c430b345f6f937eec5ebf5d14cfc4e8b43
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2021-09-02 17:39:01 -0300

    fix: remove node_env and default host

--- src/constants.ts
+++ src/constants.ts
@@ -10,9 +10,9 @@ const configService = new ConfigService();
 export const MAJOR = configService.get<string>('MAJOR') || 'dev';
 export const VERSION = configService.get<string>('VERSION') || '1';
 
-export const NODE_ENV = configService.get<string>('NODE_ENV') || 'production';
+export const NODE_ENV = configService.get<string>('NODE_ENV');
 export const APP_PORT = configService.get<number>('APP_PORT');
-export const APP_HOST = configService.get<string>('APP_HOST') || '0.0.0.0';
+export const APP_HOST = configService.get<string>('APP_HOST');
 
 export const IS_PROD = NODE_ENV === 'production';
 export const IS_TEST = NODE_ENV === 'test';
